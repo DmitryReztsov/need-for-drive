44863aeb521882f6f843eb92184b22011cd02c48
DmitryReztsov <rez_dima@mail.ru>
2022-02-17 21:56:52 +0300

    V-01 правки по флексам

--- src/components/elements/Footer/Footer.tsx
+++ src/components/elements/Footer/Footer.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import styles from './Footer.module.scss';
-import CustomLink from '../Link/CustomLink';
 
 function Footer() {
   return (
     <footer className={styles.footer}>
       <span>© 2016-2019 «Need for drive»</span>
-      <CustomLink className={styles.link}>8 (495) 234-22-44</CustomLink>
+      <a href="tel:84952342244">8 (495) 234-22-44</a>
     </footer>
   );
 }
