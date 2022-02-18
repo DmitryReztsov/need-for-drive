d2ca3ed79f416bc946bd58d668add0f48458c173
DmitryReztsov <rez_dima@mail.ru>
2022-02-18 17:26:56 +0300

    V-01_F-01 сделаны кнопки слайдера

--- src/components/elements/slider-components/Slider/Slider.tsx
+++ src/components/elements/slider-components/Slider/Slider.tsx
@@ -1,6 +1,8 @@
 import React, {useEffect, useRef, useState} from 'react';
 import styles from './Slider.module.scss';
 import Slide from '../Slide/Slide';
+import {ReactComponent as LeftArrow} from '../../../../content/svg/left-arrow.svg';
+import {ReactComponent as RightArrow} from '../../../../content/svg/right-arrow.svg';
 
 interface ContentProps {
   translate: string | number
@@ -24,6 +26,12 @@ function Slider({slides}: SliderProps): React.ReactElement {
     <div className={styles.slider}>
       <div className={styles.content} style={{transform: `translateX(-${translate}px)`, width: ref?.current?.clientWidth}}>
         {slides.map((slide, i) => <Slide refProp={ref} key={slides[i]} src={slides[i]} />)}
+      </div>
+      <div className={`${styles.arrow} ${styles.arrow_left}`}>
+        <LeftArrow />
+      </div>
+      <div className={`${styles.arrow} ${styles.arrow_right}`}>
+        <RightArrow />
       </div>
     </div>
   );
