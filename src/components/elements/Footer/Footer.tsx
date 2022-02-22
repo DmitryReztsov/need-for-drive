ef66beab3aaf44292e7fb43f2ccca3d3897f0b1b
DmitryReztsov <rez_dima@mail.ru>
2022-02-22 10:24:31 +0300

    V-02 ввел компонент контейнер для простоты верстки, рефактор главной страницы

--- src/components/elements/Footer/Footer.tsx
+++ src/components/elements/Footer/Footer.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import Container from '../Container/Container';
 import styles from './Footer.module.scss';
 
 function Footer() {
   return (
     <footer className={styles.footer}>
-      <span>© 2016-2019 «Need for drive»</span>
-      <a href="tel:84952342244">8 (495) 234-22-44</a>
+      <Container>
+        <div className={styles.body}>
+          <span>© 2016-2019 «Need for drive»</span>
+          <a href="tel:84952342244">8 (495) 234-22-44</a>
+        </div>
+      </Container>
     </footer>
   );
 }
--- src/components/elements/Main/Main.tsx
+++ src/components/elements/Main/Main.tsx
@@ -2,16 +2,21 @@ import React from 'react';
 import styles from './Main.module.scss';
 import CustomLink from '../Link/CustomLink';
 import Button from '../Button/Button';
+import Container from '../Container/Container';
 
 function Main() {
   return (
     <div className={styles.main}>
-      <h2 className={styles.title}>
-        <span>Каршеринг</span>
-        <br />
-        <span className={styles.title_green}>Need for drive</span>
-      </h2>
-      <p className={styles.text}>Поминутная аренда авто твоего города</p>
+      <Container>
+        <div className={styles.body}>
+          <h2 className={styles.title}>
+            <span>Каршеринг</span>
+            <br />
+            <span className={styles.title_green}>Need for drive</span>
+          </h2>
+          <p className={styles.text}>Поминутная аренда авто твоего города</p>
+        </div>
+      </Container>
       <CustomLink href="/order">
         <Button className={styles.button}>Забронировать</Button>
       </CustomLink>
